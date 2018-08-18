2a801cf89ffbea3dee20ec7dddbdec31801401ab
hujianguo_glb <14751946428@163.com>
2018-08-19 00:34:24 +0800

    My first commit eclipse

---
+++ src/main/java/com/hujianguo/git/HelloWord.java
@@ -0,0 +1,10 @@
+package com.hujianguo.git;
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/test/java/com/hujianguo/test/TestApi.java
@@ -0,0 +1,10 @@
+package com.hujianguo.test;
+
+public class TestApi {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
